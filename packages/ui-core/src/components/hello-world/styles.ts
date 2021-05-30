ddb07feffc77015869b3917ba773f69d6d85fb7c
Aaron <tiquismiquisss@gmail.com>
2021-05-30 13:50:55 +0200

    refactor(ui-core): hello world styles

--- packages/ui-core/src/components/hello-world/styles.ts
+++ packages/ui-core/src/components/hello-world/styles.ts
@@ -7,18 +7,18 @@ import { StyleProps } from './hello-world-props';
 export const useStyles = createUseStyles<any, StyleProps, Theme>((theme) => ({
     wrapper: {
         // color: ({ colorText }) => (colorText ? colorText : 'black'),
-        color: ({ colorText }) =>
-            colorText ? colorText : theme.variables.colors.primary,
-        backgroundColor: theme.variables.colors.background,
-        [getMinBreakpoint(theme.variables.breakpoints.sMin)]: {
+        color: ({ colorText }): string =>
+            colorText ? colorText : theme.colors.primary,
+        backgroundColor: theme.colors.background,
+        [getMinBreakpoint(theme.breakpoints.sMin)]: {
             fontSize: '16px',
             lineHeight: '20px',
         },
-        [getMinBreakpoint(theme.variables.breakpoints.mMin)]: {
+        [getMinBreakpoint(theme.breakpoints.mMin)]: {
             fontSize: '32px',
             lineHeight: '36px',
         },
-        [getMinBreakpoint(theme.variables.breakpoints.lMin)]: {
+        [getMinBreakpoint(theme.breakpoints.lMin)]: {
             fontSize: '40px',
             lineHeight: '44px',
         },
